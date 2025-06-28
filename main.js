2ca9f618976f8ebde4f80ce33c0e643b8c0a6b0e
Ahakim-xi <ahakimiii398@gmail.com>
2025-06-28 19:25:02 +0300

    complete lab

--- main.js
+++ main.js
@@ -23,3 +23,46 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+// Provided server call function — do not delete this
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
+
+// Code starts here
+
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const errorMessage = document.getElementById("modal-message");
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === "♡") {
+            heart.textContent = "♥";
+            heart.classList.add("activated-heart");
+          } else {
+            heart.textContent = "♡";
+            heart.classList.remove("activated-heart");
+          }
+        })
+        .catch((error) => {
+          errorModal.classList.remove("hidden");
+          errorMessage.textContent = error;
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
